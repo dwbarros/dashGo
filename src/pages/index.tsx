5b0b794d40c892e53c59cc9e31857ac8301de167
David Barros <dwbarros@gmail.com>
2022-02-26 20:33:56 +0000

    General and Chakra UI configuration.

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+
+  return (
+    <>
+      <Head>
+        <title>Dash Go</title>
+      </Head>
+
+      <h1>DashGo</h1>
+    </>
+  )
+}
