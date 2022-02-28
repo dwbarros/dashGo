d8d6c850c02e8cb8a58c9ab357c906418a8d6d6a
David Barros <dwbarros@gmail.com>
2022-02-28 16:15:38 +0000

    Create users page.

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,105 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Link, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+import NextLink from 'next/link';
+
+type User = {
+    id: string;
+    name: string;
+    email: string;
+    created_at: string;
+}
+
+type Data = {
+    users: User[];
+}
+
+export default function UserList() {
+
+    const data: Data = {
+        users: [{
+            id: "001",
+            name: "David",
+            email: "dwbarros@gmail.com",
+            created_at: '07/02/2022',
+        }, {
+            id: "002",
+            name: "Jesus",
+            email: "jesus@paraiso.com",
+            created_at: "22/02/2022",
+        }]
+    }
+
+    return (
+        <Box>
+            <Header />
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+                <Sidebar />
+                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                    <Flex mb="8" justify="space-between" align="center">
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+                        </Heading>
+                        <NextLink href="/users/create" passHref>
+                            <Button
+                                as="a"
+                                size="sm"
+                                fontSize="sm"
+                                colorScheme="pink"
+                                cursor="pointer"
+                                leftIcon={<Icon
+                                    as={RiAddLine}
+                                />}
+                            >
+                                Criar novo
+                            </Button>
+                        </NextLink>
+                    </Flex>
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px={["4", "4", "6"]} color="gray.300" w="8">
+                                    <Checkbox colorScheme="pink" />
+                                </Th>
+                                <Th>Usuário</Th>
+                                <Th>Data de cadastro</Th>
+                                <Th></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {data.users.map(user => (
+                                <Tr key={user.id}>
+                                    <Td px={["4", "4", "6"]}>
+                                        <Checkbox colorScheme="pink" />
+                                    </Td>
+                                    <Td>
+                                        <Box>
+                                            <Link color="purple.400">
+                                                <Text fontWeight="bold">{user.name}</Text>
+                                            </Link>
+                                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                        </Box>
+                                    </Td>
+                                    <Td>{user.created_at}</Td>
+                                    <Td>
+                                        <Button
+                                            as="a"
+                                            size="sm"
+                                            fontSize="sm"
+                                            colorScheme="purple"
+                                            cursor="pointer"
+                                            leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                        >
+                                            Editar
+                                        </Button>
+                                    </Td>
+                                </Tr>
+                            ))}
+                        </Tbody>
+                    </Table>
+                </Box>
+            </Flex>
+        </Box>
+    )
+}
